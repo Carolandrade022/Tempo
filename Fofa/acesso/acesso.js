5ed144627372c8cd76ffbbe4b5d3239f746423b9
Carolandrade022 <Carolandrade022x@gmail.com>
2023-11-11 01:08:23 -0300

    add

---
+++ Fofa/acesso/acesso.js
@@ -0,0 +1,55 @@
+const btn = document.querySelector("#btn")
+
+const usuario = document.querySelector(".usuario").value;
+
+const senha = document.querySelector(".senha").value;
+
+
+function send(){
+
+const usuario = document.querySelector(".usuario").value;
+
+const senha = document.querySelector(".senha").value;
+
+const preencha = document.querySelector("#preencha")
+
+const sucesso = document.querySelector("#sucesso")
+
+
+if(usuario.length <1 || senha.length <1){
+    
+    preencha.style.display = ('inline')
+
+    setTimeout(() => {
+
+        preencha.style.display = ('none')
+    
+        }, 1000);
+
+    return false;
+}
+
+if(usuario == "naty" && senha == "narol"){
+
+    sucesso.style.display = ('inline')
+
+    setTimeout(() => {
+
+    location.href = "file:///C:/Users/Vexus/Desktop/Login-do-meu-coracao-main/home/home.html";
+
+    }, 500);
+    
+} else{
+
+    negado.style.display = ('inline')
+
+    setTimeout(() => {
+
+        location.href = "file:///C:/Users/Vexus/Desktop/Login-do-meu-coracao-main/error/erro.html";
+    
+        }, 500);
+
+};
+
+
+};
